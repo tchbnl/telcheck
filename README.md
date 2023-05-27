1a2ff9bde909a1e8dddb468d08c43913e458a2b9
Nathan Paton <nathanpat@inmotionhosting.com>
2023-05-26 23:01:14 -0400

    Did some updates and removed cPanel dependency for additional IP checks

--- README.md
+++ README.md
@@ -21,17 +21,12 @@ and collect the information. Get delisted. That's it.
 * Gmail
 * Outlook/Hotmail
 
-Gmail _should_ work, but I haven't been able to check it against a blocked IP. I've confirmed all other hosts do work.
+Gmail _should_ work, but I haven't been able to check it against a blocked IP (it's surprisingly hard to ask for dirty IPs from hosts ¯\\\_(ツ)\_/¯). I've confirmed all other hosts do work.
 
 Support is available to check different IP addresses, as well as a verbose mode to view the entire telnet output for troubleshooting or extra information.
 
-The current version is **v0.9 R2 (Updated on 12/14/2022)**. You can check the downloaded version with `-v` or `--version`. R2 is a rewrite of the original telcheck code and includes overall "better" code quality, a redesigned output, and a verbose mode. R1 is still available and includes additional checks for Fastmail and Optimum. You can find it in the 'classic' branch. A minified version that can work on CentOS 6 is also available, albeit without support (I needed it one time).
+The current version is **v0.9 R2 (Updated on 5/26/2023)**. You can check the downloaded version with `-v` or `--version`.
 
 A minified version without comments and extra spaces is available as telcheck.min.sh. This version is designed to be pasted directly into an open Bash shell and run like normal.
 
-telcheck is released under the venerable DWYWIDC License. Do What You Want; I Don't Care. Just remember to credit me, and stop spamming so much that this script is needed. Check LICENSE for the details.
-
 I offer no warranty for this script and what it might do. You are ultimately the one responsible for the reputation of your mail server and what runs on it.
-
-## A note on Windows Terminal and telcheck.min
-For some reason Windows Terminal has a hard time handling pasted text longer than about 100 lines. I recommend splitting the code up into two pastes in case of problems.
--- telcheck.sh
+++ telcheck.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 # telcheck: Check for email blocks with telnet
-# Nathan Paton <code@tchbnl.net>
-# v0.9 R2 (Updated on 12/14/2022)
+# Nathan Paton <me@tchbnl.net>
+# v0.9 R2 (Updated on 5/26/2023)
 
 # Some of this stuff won't work on Bash versions before 4.2
+# Use telnet.el6.min.sh for old servers
 if [[ "${BASH_VERSINFO[0]}${BASH_VERSINFO[1]}" -lt "42" ]]; then
   echo "telcheck requires at least Bash 4.2 to work. Your version is ${BASH_VERSION}."
   exit
 fi
 
 # It's rare, but sometimes telnet isn't installed
-if ! command -v telnet >/dev/null; then
+if ! command -v telnet >/dev/null 2>&1; then
   echo "telcheck requires telnet to work. Hence the name 'tel(net)check'."
   exit
 fi
 
-# In case of repeated telcheck.min runs, we unset some variables first
+# Unset some variables in case this is telcheck.min.sh
 unset SOURCE_IP
 unset VERBOSE
 unset IS_BLOCKED
 
-# Version and last update date
-VERSION="telcheck 0.9 R2 (Updated on 12/14/2022)"
+# Version information
+VERSION="telcheck 0.9 R2 (Updated on 5/26/2023)"
 
-# Text formatting
+# Nice text formatting
 TEXT_BOLD="\e[1m"
 TEXT_RESET="\e[0m"
 
@@ -40,12 +41,12 @@ USAGE: telcheck [-b IP]
 Note: Do not abuse this script! Frequent checks can make things worse. Run once
 and collect the information. Get delisted. That's it."
 
-# Left is the host and display text for the check output. Right is the actual
-# MX record run through telnet.
+# Left is the host and display text for the check output. Right is the actual MX
+# record we run through telnet.
 HOSTS=("Yahoo! (+ AOL and Verizon), mta5.am0.yahoodns.net"
        "ATT, al-ip4-mx-vip1.prodigy.net"
        "Comcast, mx1a1.comcast.net"
-       "Cox\U2122, cxr.mx.a.cloudfilter.net"
+       "Cox™, cxr.mx.a.cloudfilter.net"
        "EarthLink, mx01.oxsus-vadesecure.net"
        "Gmail, gmail-smtp-in.l.google.com"
        "Outlook (+ Hotmail), outlook-com.olc.protection.outlook.com")
@@ -88,8 +89,7 @@ while [[ "${#}" -gt 0 ]]; do
   esac
 done
 
-# Make sure the IP being used can be bound to and can talk to a local mail
-# server. If it can't, we bail here.
+# Make sure the IP being used can be bound to and can talk to a local mail server
 if (sleep 1; echo "QUIT") | telnet ${SOURCE_IP:+-b ${SOURCE_IP} }127.0.0.1 25 2>&1 \
   | grep -Eiq "unable to connect|can't assign|cannot assign|nodename nor servname provided|couldn't get address|could not get address|name or service not known"; then
   echo -e "${TEXT_BOLD}Error:${TEXT_RESET} IP address is invalid or not" \
@@ -100,16 +100,24 @@ fi
 
 # This uses the cPanel API to fetch all usable IPs on the server. A check for
 # the 'whmapi1' command is done first to make sure this will work.
-if [[ -x /usr/local/cpanel/bin/whmapi1 ]]; then
-  if whmapi1 listips | sed '/'"$(hostname -i)"'/d' | grep -iq "public_ip:"; then
-    echo -e "${TEXT_BOLD}Found one or more additional IP addresses:${TEXT_RESET}"
-    whmapi1 listips | sed '/'"$(hostname -i)"'/d' | grep -i "public_ip:" \
-    | awk -F ': ' '{print "* " $2}'
-    echo -e "You can check a different IP address with '-b IP'.\n"
-  fi
+# TODO: Rewrite this to not require cPanel
+#if [[ -x /usr/local/cpanel/bin/whmapi1 ]]; then
+#  if whmapi1 listips | sed '/'"$(hostname -i)"'/d' | grep -iq "public_ip:"; then
+#    echo -e "${TEXT_BOLD}Found one or more additional IP addresses:${TEXT_RESET}"
+#    whmapi1 listips | sed '/'"$(hostname -i)"'/d' | grep -i "public_ip:" \
+#    | awk -F ': ' '{print "* " $2}'
+#    echo -e "You can check a different IP address with '-b IP'.\n"
+#  fi
+#fi
+
+# Get all non-local IPs on the server
+# Yes I swear I'll update this for IPv6 soon
+if [[ "$(hostname -I | xargs -n 1 | grep -Evc '^127.|^10.|^172.|^192.')" -gt 1 ]]; then
+  echo -e "${TEXT_BOLD}Found one or more additional IP addresses:${TEXT_RESET}"
+  hostname -I | xargs -n 1 | grep -Ev '^127.|^10.|^172.|^192.' | awk '{print "* " $0}'
+  echo -e "You can check a different IP address with '-b IP'.\n"
 fi
 
-# Literally a waste of SLOC
 # Random text shown when starting a check
 WAIT_TEXT=("Reticulating splines"
            "Enumerating beagles"
@@ -120,14 +128,14 @@ WAIT_TEXT=("Reticulating splines"
 echo -e "${TEXT_BOLD}${SOURCE_IP:-"$(hostname -i)"}${TEXT_RESET}"
 echo -e "Please wait. ${WAIT_TEXT[$((RANDOM % ${#WAIT_TEXT[@]}))]}...\n"
 
-# List of terms to grep with further down
+# List of terms we grep for further down
 BAD_WORDS="banned|blacklist|blacklisted|block|blocklisted|denied|dnsbl|dnsrbl|found on one or more|invaluement|ivmsip|is on a|not allowed|rbl|rejected|sorbs|spamcop|spamhaus"
 
-# Behold! The telcheck. We send a set of commands to telnet against each host
-# server. In most cases a host won't report a block until we at least set a
-# from address. We sleep between each command to give the host enough time to
-# respond. Setting this below 1.5 tends to break some host checks.
-telcheck_cmd() {
+# The actual check. We connect to the mail server and run some commands - most hosts
+# won't tell us if we're blocked until we set our from address. A delay between each
+# command ensures there's enough time for the mail server to respond.
+# I know this is slow. Setting sleep below 1.5 causes wonkiness with some mail hosts
+telcheckit() {
   (sleep 1.5; echo "EHLO $(hostname)"; sleep 1.5; echo "MAIL FROM: <root@$(hostname)>"; sleep 1.5; echo "QUIT") \
   | telnet ${SOURCE_IP:+-b ${SOURCE_IP} }"${*}" 25 2>&1
 }
@@ -138,11 +146,11 @@ for HOST in "${HOSTS[@]}"; do
 
   # We variablize the telcheck for further grepping below
   # The weird variable name is to make this text line up with the stuff above
-  TRESULT="$(telcheck_cmd "$(echo "${HOST}" | awk -F ', ' '{print $2}')")"
+  TRESULT="$(telcheckit "$(echo "${HOST}" | awk -F ', ' '{print $2}')")"
 
   # And here are the results of that greppage
   if echo "${TRESULT}" | grep -Eiq "${BAD_WORDS}"; then
-    echo -e "\U26D4 Fail"
+    echo -e "⛔ Fail"
     # We set IS_BLOCKED for the result summary at the end of thsi script
     IS_BLOCKED="Yes"
 
@@ -151,13 +159,14 @@ for HOST in "${HOSTS[@]}"; do
       echo "${TRESULT}" | grep -Ei "${BAD_WORDS}"
     fi
   else
-    echo -e "\U1F44D Pass"
+    echo -e "👍 Pass"
   fi
 
   # Because if VERBOSE is set, we show the entire telnet output instead
   if [[ -v VERBOSE ]]; then
     echo "${TRESULT}"
   fi
+
   echo
 done
 
